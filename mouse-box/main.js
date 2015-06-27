adfce0a12023b60c0812fa07fc40b2751c12b698
Raine Virta <raine.virta@gmail.com>
2015-06-27 17:21:54 +0300

    add mouse-box

---
+++ mouse-box/main.js
@@ -0,0 +1,33 @@
+const flyd = require('flyd');
+const {curry, pipe, always, merge, props, apply} = require('ramda');
+
+const mouse$ = (function() {
+  const s = flyd.stream();
+  window.addEventListener('mousemove', (ev) => {
+    s({ x: ev.x, y: ev.y });
+  });
+  return s;
+}());
+
+const init = always({
+  x: 0,
+  y: 0
+});
+
+const setPos = curry((elem, left, top) => {
+  elem.style.left = left + 'px';
+  elem.style.top  = top + 'px';
+});
+
+const centerToElem = curry((elem, coords) => {
+  const [w, h] = [elem.offsetWidth, elem.offsetHeight];
+  return {
+    x: coords.x - w/2,
+    y: coords.y - h/2
+  };
+});
+
+const box = document.getElementById('box');
+const render = pipe(centerToElem(box), props(['x', 'y']), apply(setPos(box)));
+const model$ = flyd.scan(merge, init(), mouse$);
+flyd.on(render, model$);
