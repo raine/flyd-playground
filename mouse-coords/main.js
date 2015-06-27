3ee005b49e1194c54f6e8b4107a42bb6b366a755
Raine Virta <raine.virta@gmail.com>
2015-06-27 16:50:51 +0300

    init

---
+++ mouse-coords/main.js
@@ -0,0 +1,15 @@
+const flyd = require('flyd');
+const {curry, pipe} = require('ramda');
+
+const setProp = curry((obj, prop, value) => obj[prop] = value);
+const mouse$ = (function() {
+  const s = flyd.stream();
+  window.addEventListener('mousemove', (ev) => 
+    s({ x: ev.x, y: ev.y }));
+  return s;
+}());
+
+const formatCoords = (coords) => `(${coords.x}, ${coords.y})`;
+const render = pipe(formatCoords, setProp(document.body, 'innerHTML'));
+
+flyd.on(render, mouse$);
