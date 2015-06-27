b15da23868686dc636525ab50da34da7969b8809
Raine Virta <raine.virta@gmail.com>
2015-06-27 20:55:00 +0300

    publish: add build
    generated from commit a5d306cafb6db2be4c7240749731815298a5738e

---
+++ fps/main.js
@@ -0,0 +1,19 @@
+const flyd = require('flyd');
+const {curry, pipe, invoker} = require('ramda');
+
+const fps$ = (function() {
+  var oldTime = +new Date;
+  const s = flyd.stream();
+  (function frame(time) {
+    window.requestAnimationFrame(frame);
+    s(1000 / (time - oldTime));
+    oldTime = time;
+  }());
+  return s;
+}());
+
+const toFixed = invoker(1, 'toFixed');
+const setProp = curry((obj, prop, value) => obj[prop] = value);
+const render = pipe(toFixed(1), setProp(document.body, 'innerHTML'));
+
+flyd.on(render, fps$);
